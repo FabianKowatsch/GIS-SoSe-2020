8f5d428179d26d70584142b3c8bd2213e5cebca7
Fabian Kowatsch <f.kowatsch2000@gmail.com>
2020-05-31 11:59:42 +0200

    ts geändert

--- Aufgabe05/data.js
+++ Aufgabe05/data.js
@@ -1,47 +1,26 @@
 "use strict";
-let liste = [
-    { name: "1m² Rasenfläche", bild: "\"rasen.jpg\"", desc: "Die seltenste Pflanze der Welt!", preis: 5, kat: 1, ad: false },
-    { name: "Rosenstrauch", bild: "\"rose.jpg\"", desc: "Perfekt zum verschenken!", preis: 15, kat: 1, ad: false },
-    { name: "Lavendel", bild: "\"lavendel.jpg\"", desc: "für den guten Duft im Garten:", preis: 3, kat: 1, ad: false },
-    { name: "Sonnenblume", bild: "\"sunflower.jpg\"", desc: "Multifunktionspflanze", preis: 4, kat: 1, ad: false },
-    { name: "Nutzhanf", bild: "\"hanf.jpg\"", desc: "Hier gibt es selbstverständlich nur die männlichen Pflanzen ;)", preis: 15, kat: 1, ad: false },
-    { name: "Salatsetzling", bild: "\"salat.jpg\"", desc: "eine Kuriosität!", preis: 2, kat: 1, ad: false },
-    { name: "Gewächshaus", bild: "\"greenhouse.jpg\"", desc: "Salat wächst hier am besten!", preis: 600, kat: 2, ad: false },
-    { name: "Gartenhaus", bild: "\"gartenhaus.jpg\"", desc: "zum verstauen der Werkzeuge", preis: 999, kat: 2, ad: false },
-    { name: "Gartenstuhl", bild: "\"chair.jpg\"", desc: "Die perfekte Sitzgelegenheit!", preis: 30, kat: 2, ad: true },
-    { name: "Massivholztisch", bild: "\"tisch.png\"", desc: "Der hält ein paar Generationen lang!", preis: 200, kat: 2, ad: true },
-    { name: "Gartenbank", bild: "\"bank.jpg\"", desc: "sehr gemütlich", preis: 250, kat: 2, ad: false },
-    { name: "Gartenmobiliar-Set", bild: "\"set.jpg\"", desc: "Wieso einzeln kaufen?", preis: 199, kat: 2, ad: false },
-    { name: "Biertischgarnitur", bild: "\"biertisch.jpg\"", desc: "Der Klassiker für jedes Fest", preis: 60, kat: 2, ad: false },
-    { name: "Schaufel", bild: "\"schaufel.jpg\"", desc: "Die bricht nicht!", preis: 15, kat: 3, ad: true },
-    { name: "Gartenschaufel", bild: "\"gartenschaufel.jpg\"", desc: "Für die Feinarbeit", preis: 5, kat: 3, ad: false },
-    { name: "Schubkarren", bild: "\"schubkarrenl.jpg\"", desc: "Der ist in echt noch rostiger!", preis: 40, kat: 3, ad: true },
-    { name: "Rechen", bild: "\"rechen.jpg\"", desc: "Raus mit de Viecher!", preis: 15, kat: 3, ad: false },
-    { name: "Laubrechen", bild: "\"laubrechen.jpg\"", desc: "ein Rechen", preis: 15, kat: 3, ad: false },
-    { name: "Strohbesen", bild: "\"strohbesen.jpg\"", desc: "der altbewährte Bestseller", preis: 5, kat: 3, ad: false }
-];
-let b = document.createElement("p");
-document.body.appendChild(b);
-b.innerHTML = "neues E";
-let a = document.getElementById("kat1");
-a.innerHTML = "a";
-let i;
-for (i = 0; i < liste.length; i++) {
-    switch (liste[i].kat) {
-        case 1:
-            let element1 = document.getElementById("container1");
-            element1.innerHTML = "<div class=\"item\">" + liste[i].name + "<img src=" + liste[i].bild + "alt=\"Bild\"> <p class=\"desc\">" + liste[i].desc + "</p><p>pro Stück: <b class=\"preis\">" + liste[i].preis + "</b></p><button name=\"Kaufen\">Kaufen</button></div>";
-            break;
-        case 2:
-            let element2 = document.getElementById("container2");
-            element2.innerHTML = "<div class=\"item\">" + liste[i].name + "<img src=" + liste[i].bild + "alt=\"Bild\"> <p class=\"desc\">" + liste[i].desc + "</p><p>pro Stück: <b class=\"preis\">" + liste[i].preis + "</b></p><button name=\"Kaufen\">Kaufen</button></div>";
-            break;
-        case 3:
-            let element3 = document.getElementById("container3");
-            element3.innerHTML = "<div class=\"item\">" + liste[i].name + "<img src=" + liste[i].bild + "alt=\"Bild\"> <p class=\"desc\">" + liste[i].desc + "</p><p>pro Stück: <b class=\"preis\">" + liste[i].preis + "</b></p><button name=\"Kaufen\">Kaufen</button></div>";
-        default:
-            console.log("Fehler");
-            break;
-    }
-}
+var Aufgabe05;
+(function (Aufgabe05) {
+    Aufgabe05.liste = [
+        { name: "1m² Rasenfläche", bild: "rasen.jpg", desc: "Die seltenste Pflanze der Welt!", preis: 5, kat: 1, ad: false },
+        { name: "Rosenstrauch", bild: "rose.jpg", desc: "Perfekt zum verschenken!", preis: 15, kat: 1, ad: false },
+        { name: "Lavendel", bild: "lavendel.jpg", desc: "für den guten Duft im Garten:", preis: 3, kat: 1, ad: false },
+        { name: "Sonnenblume", bild: "sunflower.jpg", desc: "Multifunktionspflanze", preis: 4, kat: 1, ad: false },
+        { name: "Nutzhanf", bild: "hanf.jpg", desc: "Hier gibt es selbstverständlich nur die männlichen Pflanzen ;)", preis: 15, kat: 1, ad: false },
+        { name: "Salatsetzling", bild: "salat.jpg", desc: "eine Kuriosität!", preis: 2, kat: 1, ad: false },
+        { name: "Gewächshaus", bild: "greenhouse.jpg", desc: "Salat wächst hier am besten!", preis: 600, kat: 2, ad: false },
+        { name: "Gartenhaus", bild: "gartenhaus.jpg", desc: "zum verstauen der Werkzeuge", preis: 999, kat: 2, ad: false },
+        { name: "Gartenstuhl", bild: "chair.jpg", desc: "Die perfekte Sitzgelegenheit!", preis: 30, kat: 2, ad: true },
+        { name: "Massivholztisch", bild: "tisch.png", desc: "Der hält ein paar Generationen lang!", preis: 200, kat: 2, ad: true },
+        { name: "Gartenbank", bild: "bank.jpg", desc: "sehr gemütlich", preis: 250, kat: 2, ad: false },
+        { name: "Gartenmobiliar-Set", bild: "set.jpg", desc: "Wieso einzeln kaufen?", preis: 199, kat: 2, ad: false },
+        { name: "Biertischgarnitur", bild: "biertisch.jpg", desc: "Der Klassiker für jedes Fest", preis: 60, kat: 2, ad: false },
+        { name: "Schaufel", bild: "schaufel.jpg", desc: "Die bricht nicht!", preis: 15, kat: 3, ad: true },
+        { name: "Gartenschaufel", bild: "gartenschaufel.jpg", desc: "Für die Feinarbeit", preis: 5, kat: 3, ad: false },
+        { name: "Schubkarren", bild: "schubkarrenl.jpg", desc: "Der ist in echt noch rostiger!", preis: 40, kat: 3, ad: true },
+        { name: "Rechen", bild: "rechen.jpg", desc: "Raus mit de Viecher!", preis: 15, kat: 3, ad: false },
+        { name: "Laubrechen", bild: "laubrechen.jpg", desc: "ein Rechen", preis: 15, kat: 3, ad: false },
+        { name: "Strohbesen", bild: "strohbesen.jpg", desc: "der altbewährte Bestseller", preis: 5, kat: 3, ad: false }
+    ];
+})(Aufgabe05 || (Aufgabe05 = {}));
 //# sourceMappingURL=data.js.map
---
+++ Aufgabe05/function.js
@@ -0,0 +1,74 @@
+"use strict";
+var Aufgabe05;
+(function (Aufgabe05) {
+    for (let i = 0; i < Aufgabe05.liste.length; i++) {
+        switch (Aufgabe05.liste[i].kat) {
+            case 1:
+                let newDiv = document.createElement("div");
+                newDiv.setAttribute("class", "item");
+                newDiv.innerHTML = Aufgabe05.liste[i].name;
+                document.getElementById("container1")?.appendChild(newDiv);
+                let bild = document.createElement("img");
+                bild.setAttribute("src", Aufgabe05.liste[i].bild);
+                bild.setAttribute("alt", Aufgabe05.liste[i].name);
+                newDiv.appendChild(bild);
+                let desc = document.createElement("p");
+                desc.setAttribute("class", Aufgabe05.liste[i].bild);
+                desc.innerHTML = Aufgabe05.liste[i].desc;
+                newDiv.appendChild(desc);
+                let txt = document.createElement("p");
+                txt.innerHTML = "pro Stück";
+                newDiv.appendChild(txt);
+                let preis = document.createElement("b");
+                preis.setAttribute("class", "preis");
+                preis.innerHTML = "" + Aufgabe05.liste[i].preis + "";
+                txt.appendChild(preis);
+                break;
+            case 2:
+                let newDiv2 = document.createElement("div");
+                newDiv2.setAttribute("class", "item");
+                newDiv2.innerHTML = Aufgabe05.liste[i].name;
+                document.getElementById("container1")?.appendChild(newDiv2);
+                let bild2 = document.createElement("img");
+                bild2.setAttribute("src", Aufgabe05.liste[i].bild);
+                bild2.setAttribute("alt", Aufgabe05.liste[i].name);
+                newDiv2.appendChild(bild2);
+                let desc2 = document.createElement("p");
+                desc2.setAttribute("class", Aufgabe05.liste[i].bild);
+                desc2.innerHTML = Aufgabe05.liste[i].desc;
+                newDiv2.appendChild(desc2);
+                let txt2 = document.createElement("p");
+                txt2.innerHTML = "pro Stück";
+                newDiv2.appendChild(txt2);
+                let preis2 = document.createElement("b");
+                preis2.setAttribute("class", "preis");
+                preis2.innerHTML = "" + Aufgabe05.liste[i].preis + "";
+                txt2.appendChild(preis2);
+                break;
+            case 3:
+                let newDiv3 = document.createElement("div");
+                newDiv3.setAttribute("class", "item");
+                newDiv3.innerHTML = Aufgabe05.liste[i].name;
+                document.getElementById("container1")?.appendChild(newDiv3);
+                let bild3 = document.createElement("img");
+                bild3.setAttribute("src", Aufgabe05.liste[i].bild);
+                bild3.setAttribute("alt", Aufgabe05.liste[i].name);
+                newDiv3.appendChild(bild3);
+                let desc3 = document.createElement("p");
+                desc3.setAttribute("class", Aufgabe05.liste[i].bild);
+                desc3.innerHTML = Aufgabe05.liste[i].desc;
+                newDiv3.appendChild(desc3);
+                let txt3 = document.createElement("p");
+                txt3.innerHTML = "pro Stück";
+                newDiv3.appendChild(txt3);
+                let preis3 = document.createElement("b");
+                preis3.setAttribute("class", "preis");
+                preis3.innerHTML = "" + Aufgabe05.liste[i].preis + "";
+                txt3.appendChild(preis3);
+            default:
+                console.log("Fehler");
+                break;
+        }
+    }
+})(Aufgabe05 || (Aufgabe05 = {}));
+//# sourceMappingURL=function.js.map
