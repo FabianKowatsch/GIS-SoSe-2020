2c41617c5d9a118cfcee736632948f6167cc375e
Fabian Kowatsch <f.kowatsch2000@gmail.com>
2020-06-03 18:49:41 +0200

    a1 für ein item

--- Aufgabe06/function.js
+++ Aufgabe06/function.js
@@ -1,6 +1,7 @@
 "use strict";
 var Aufgabe06;
 (function (Aufgabe06) {
+    //Funktion zum erstellen der Artikel
     function addItem(x, i, as) {
         let newDiv = document.createElement("div");
         newDiv.setAttribute("class", "item");
@@ -37,9 +38,11 @@ var Aufgabe06;
         txt.appendChild(preis);
         let knopf = document.createElement("button");
         knopf.setAttribute("name", "kaufen");
+        knopf.setAttribute("data-price", "" + Aufgabe06.liste[i].preis);
         knopf.innerHTML = "Kaufen";
         newDiv.appendChild(knopf);
     }
+    //Seitenaufbau  
     for (let i = 0; i < Aufgabe06.liste.length; i++) {
         switch (Aufgabe06.liste[i].kat) {
             case 1:
@@ -51,7 +54,6 @@ var Aufgabe06;
             case 3:
                 addItem(3, i, false);
             default:
-                console.log("Fehler");
                 break;
         }
         if (Aufgabe06.liste[i].ad) {
@@ -61,6 +63,33 @@ var Aufgabe06;
             if (Aufgabe06.liste[i].kat == 3) {
                 addItem(3, i, true);
             }
+        }
+    }
+    let target = document.querySelector("button");
+    target.addEventListener("click", hndClick);
+    let zähler = 0;
+    let summe = 0;
+    function hndClick(_onclick) {
+        if (zähler == 0) {
+            let cnt = document.createElement("span");
+            cnt.setAttribute("id", "counter");
+            document.getElementById("pay")?.appendChild(cnt);
+            zähler += 1;
+            cnt.innerHTML = `0${zähler}`;
+            let prs = target.dataset.price;
+            summe = summe + parseInt(prs);
+            console.log(summe);
+        }
+        else {
+            zähler += 1;
+            let cnt2 = document.getElementById("counter");
+            if (zähler <= 9 && zähler > 0)
+                cnt2.innerHTML = `0${zähler}`;
+            if (zähler > 9)
+                cnt2.innerHTML = "" + zähler;
+            let prs = target.dataset.price;
+            summe = summe + parseInt(prs);
+            console.log(summe);
         }
     }
 })(Aufgabe06 || (Aufgabe06 = {}));
--- Aufgabe06/function.ts
+++ Aufgabe06/function.ts
@@ -1,5 +1,6 @@
 namespace Aufgabe06 {
-      
+
+//Funktion zum erstellen der Artikel
     function addItem(x: number, i: number, as: boolean): void {
         let newDiv: HTMLElement = document.createElement("div");
         newDiv.setAttribute("class", "item");
@@ -36,9 +37,11 @@ namespace Aufgabe06 {
         txt.appendChild(preis);
         let knopf: HTMLElement = document.createElement("button");
         knopf.setAttribute("name", "kaufen");
+        knopf.setAttribute("data-price", "" + liste[i].preis);
         knopf.innerHTML = "Kaufen";
         newDiv.appendChild(knopf);
-    }    
+    } 
+    //Seitenaufbau  
     for (let i: number = 0; i < liste.length; i++) {
         
     switch (liste[i].kat) {
@@ -51,7 +54,7 @@ namespace Aufgabe06 {
         case 3:
             addItem(3, i, false);
         default:
-            console.log("Fehler");
+            
             break;
     }
     if (liste[i].ad) {
@@ -62,5 +65,36 @@ namespace Aufgabe06 {
             addItem(3, i, true);
         }
     }
+    }
+    let target: HTMLButtonElement = document.querySelector("button") as HTMLButtonElement;
+    target.addEventListener("click", hndClick);
+    let zähler: number = 0;
+    let summe: number = 0;
+
+    function hndClick(_onclick: Event): void {
+    
+    if (zähler == 0) {
+    let cnt: HTMLElement = document.createElement("span");
+    cnt.setAttribute("id", "counter");
+    document.getElementById("pay")?.appendChild(cnt);
+    zähler += 1;
+    cnt.innerHTML = `0${zähler}`; 
+    let prs: string = target.dataset.price as string;
+    summe = summe + parseInt(prs); 
+    console.log(summe);
+}
+    
+    else {  
+    zähler += 1;
+    let cnt2: HTMLElement = document.getElementById("counter") as HTMLElement;
+    if (zähler <= 9 && zähler > 0)
+    cnt2.innerHTML = `0${zähler}`;
+    if (zähler > 9) 
+        cnt2.innerHTML = "" + zähler;
+    let prs: string = target.dataset.price as string;
+    summe = summe + parseInt(prs);
+    console.log(summe);
+    
+}
     }
 }
