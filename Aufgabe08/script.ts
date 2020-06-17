1b94f0be6118786b107e2c8366d28833e2977b28
Fabian Kowatsch <f.kowatsch2000@gmail.com>
2020-06-17 12:33:18 +0200

    a8.1

--- Aufgabe08/script.ts
+++ Aufgabe08/script.ts
@@ -1,9 +1,28 @@
 import * as Http from "http";
-import * as Url from "url";
 
-export namespace Aufgabe8 {
-    let i: number = 1;
-    console.log(i);
-    console.log(Http);
-    console.log(Url);
+export namespace A08Server {
+  console.log("Starting server");
+  let port: number = Number(process.env.PORT);
+  if (!port)
+    port = 8100;
+
+  let server: Http.Server = Http.createServer();
+  server.addListener("request", handleRequest);
+  server.addListener("listening", handleListen);
+  server.listen(port);
+
+  function handleListen(): void {
+    console.log("Listening");
+  }
+
+  function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+    console.log("I hear voices!");
+
+    _response.setHeader("content-type", "text/html; charset=utf-8");
+    _response.setHeader("Access-Control-Allow-Origin", "*");
+
+    _response.write(_request.url);
+
+    _response.end();
+  }
 }
