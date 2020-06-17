8a28b1bd541ddb93b09d2195c76be5bec0c885f0
Fabian Kowatsch <f.kowatsch2000@gmail.com>
2020-06-17 11:37:18 +0200

    script erstellt

---
+++ Aufgabe08/script.ts
@@ -0,0 +1,9 @@
+import * as Http from "http";
+import * as Url from "url";
+
+export namespace Aufgabe8 {
+    let i: number = 1;
+    console.log(i);
+    console.log(Http);
+    console.log(Url);
+}
