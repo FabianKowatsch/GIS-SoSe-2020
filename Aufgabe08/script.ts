d6296a32611cd6a2d6391ee229a51549ef96081e
Fabian Kowatsch <f.kowatsch2000@gmail.com>
2020-06-20 11:54:58 +0200

    a8 final

--- Aufgabe08/script.ts
+++ Aufgabe08/script.ts
@@ -6,7 +6,7 @@ button.addEventListener("click", hndClick);
 
 function hndClick(_event: Event): void { 
     let formData: FormData = new FormData(document.forms[0]);
-    let url: string = "http://localhost:8100";
+    let url: string = "https://fabiankowatschgis.herokuapp.com/";
     let query: URLSearchParams = new URLSearchParams(<any>formData);
     url = url + "?" + query.toString();
     communicate(url);
