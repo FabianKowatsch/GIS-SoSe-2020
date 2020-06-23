2fe03e29a8c75d17b22017059b887492ab01a9de
Fabian Kowatsch <f.kowatsch2000@gmail.com>
2020-06-23 19:39:50 +0200

    / im link entfernt

--- Aufgabe08/script.js
+++ Aufgabe08/script.js
@@ -5,13 +5,13 @@ var Aufgabe08;
     button.addEventListener("click", hndClick);
     function hndClick(_event) {
         let formData = new FormData(document.forms[0]);
-        let url = "http://localhost:8100";
+        let url = "https://fabiankowatschgis.herokuapp.com";
         let query = new URLSearchParams(formData);
         url = url + "?" + query.toString();
         communicate(url);
     }
     async function communicate(_url) {
-        let response = await fetch(_url, { method: "get" });
+        let response = await fetch(_url);
         let responsestring = await response.text();
         console.log("Antwort: " + responsestring);
     }
--- Aufgabe08/script.ts
+++ Aufgabe08/script.ts
@@ -6,14 +6,14 @@ button.addEventListener("click", hndClick);
 
 function hndClick(_event: Event): void { 
     let formData: FormData = new FormData(document.forms[0]);
-    let url: string = "https://fabiankowatschgis.herokuapp.com/";
+    let url: string = "https://fabiankowatschgis.herokuapp.com";
     let query: URLSearchParams = new URLSearchParams(<any>formData);
     url = url + "?" + query.toString();
     communicate(url);
 
 }
 async function communicate(_url: RequestInfo): Promise<void> {
-    let response: Response = await fetch(_url, { method: "get" });
+    let response: Response = await fetch(_url);
     let responsestring: String = await response.text();
     console.log("Antwort: " + responsestring);
   }
