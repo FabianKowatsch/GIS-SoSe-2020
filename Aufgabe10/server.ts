c6834100f86a364129bdf7b00e740b9a101113e4
Fabian Kowatsch <f.kowatsch2000@gmail.com>
2020-07-01 16:22:32 +0200

    a10 final

---
+++ Aufgabe10/server.ts
@@ -0,0 +1,2 @@
+//mongodb+srv://User:<password>@gisfabiankowatsch.hc0v1.mongodb.net/<dbname>?retryWrites=true&w=majority
+//https://mongodbnetbrowser.herokuapp.com/?u=User&p=irgendeinpasswort1234&a=gisfabiankowatsch.hc0v1.mongodb.net&n=Test&c=Students
